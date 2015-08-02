54d1615825c8d1514931590e437ab0c0f8eaadba
shitalgodara <shital@trumplab.com>
2015-08-02 10:50:24 +0530

    removed file size reduced from 112 to 82 Kb of dashboard.js removed file size reduced from 21 to 14 Kb of dashboard.html session error handeled in both file file shift->faq,terms,privacy links adjsut according->local link not http ones title added in all author,revisit meta tags added keyword,description favicon shortcut and bookmark logout handled removed lat long not much useful moved from dashboard to login page of modal,os mailpdf amazon request add classcode in mailpdf

--- js/login.js
+++ js/login.js
@@ -9,6 +9,43 @@ var now;
 var later;
 var x1;
 
+var nameOffset,verOffset;
+
+var nAgt = navigator.userAgent;
+var browserName  = navigator.appName;
+// In Opera, the true version is after "Opera" or after "Version"
+if ((verOffset=nAgt.indexOf("Opera"))!=-1) {
+   browserName = "Opera";
+  
+}
+// In MSIE, the true version is after "MSIE" in userAgent
+else if ((verOffset=nAgt.indexOf("MSIE"))!=-1) {
+   browserName = "Microsoft Internet Explorer";
+  
+}
+// In Chrome, the true version is after "Chrome" 
+else if ((verOffset=nAgt.indexOf("Chrome"))!=-1) {
+   browserName = "Chrome";
+  
+}
+// In Safari, the true version is after "Safari" or after "Version" 
+else if ((verOffset=nAgt.indexOf("Safari"))!=-1) {
+   browserName = "Safari";
+  
+}
+// In Firefox, the true version is after "Firefox" 
+else if ((verOffset=nAgt.indexOf("Firefox"))!=-1) {
+    browserName = "Firefox";
+    
+}
+// In most other browsers, "name/version" is at the end of userAgent 
+else if ( (nameOffset=nAgt.lastIndexOf(' ')+1) < (verOffset=nAgt.lastIndexOf('/')) ) {
+    browserName = nAgt.substring(nameOffset,verOffset);
+   
+    if (browserName.toLowerCase()==browserName.toUpperCase()) {
+       browserName = navigator.appName;
+    }
+}
 
 function hide_alert(e){
     var id = event.target.id;
@@ -263,59 +300,22 @@ function login_verify(){
 var  x1 = document.getElementById("email_id").value;
 var  x2 =document.getElementById("password").value;	
 	
-	
-Parse.User.logIn(x1, x2, {
-  success: function(user) {
+	//call with brower browserName and web
+
+Parse.Cloud.run('appEnter',{email:x1,password:x2,os:"web",model:browserName},{
+  success: function(results) {
 			var currentUser = Parse.User.current();
 			if (currentUser) {
 				var role = currentUser.get("role");
 				
 				if(role=="teacher"){
 					window.location = "dashboard.html";
-					
 				}
-				else{
-										
-										var modal_class=  document.getElementById("alerts");
-										modal_class.addEventListener("click", function(){hide_alert()}, false);
-									    var next =  document.getElementById("next");									 
-										//var msg =  document.getElementById("alert_details");
-										//var input1 =document.getElementById("enter_email");
-										//var input2 =document.getElementById("enter_password");
-										//var prefix =document.getElementById("fixed");
-										//var img =document.getElementById("add_alert_info");
-										//var back = document.getElementById("back");
-										//var sel_number = document.getElementById("select_number");
-										//var sel_email = document.getElementById("select_email");
-										//img.src="icons/error.png";
-										//img.style.display="block";
-										//input1.style.display ="none";
-										//next.style.display = "block";
-										//input2.style.display ="none";
-										//prefix.style.display = "none";
-										//sel_number.style.display ="none";
-									    //sel_email.style.display ="none";
-										//back.style.display = "none";
-										document.getElementById("alert_title_text").innerHTML ="TEACHER LOGIN ONLY";
-										document.getElementById("alert_title").style.background = "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAP0lEQVQYV2NkIAL8P8j+n5GQOrAi+5+MeBXCFIEMw6kQWRFOheiKsCrEpghDIS5FKArxKYIrJKQIrJAYRSCFACC1JtEjjo+nAAAAAElFTkSuQmCC) repeat";
-									    document.getElementById("code_text").innerHTML = "Login facility is available for teachers only. <br> Please use our app to login as parent or student";
-										$("#code_text").show();
-										setTimeout(function(){$('#code_text').hide();
-										document.getElementById("alert_title").style.background = "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAPElEQVQYV2NkIA4YMxKhzpiBgeEsIYVgRSDD8CmEK8KnEEURLoUYirApxKoIXSFORcgK8SqCKSSoCKQQALilCT6rrB3mAAAAAElFTkSuQmCC) repeat";
-										document.getElementById("alert_title_text").innerHTML ="LOGIN";
-										$("#add_alert_info").hide();
-										$("#code_text").hide();
-										},7000);
-										next.innerHTML ="DISMISS";
-										//back.innerHTML ="BACK";
-										next.setAttribute("onclick","toggleSlider()");
-										//back.setAttribute("onclick","");
-					
-				}
+				
 			}
 			
   },
-  error: function(user, error) {
+  error: function(error) {
    
 									var modal_class=  document.getElementById("alerts");
 									modal_class.addEventListener("click", function(){hide_alert()}, false);
@@ -455,7 +455,11 @@ function generate_code_login(num){
 			}
 			
 		},
-		error: function(error){console.log(error.code);
+		error: function(error){
+if(error.code==209){
+	window.location="index.html";
+}
+			console.log(error.code);
 		
 			
 										$('#login').hide();
@@ -520,7 +524,11 @@ function generate_code(num){
 			}
 			
 		},
-		error: function(error){console.log(error.code);
+		error: function(error){
+if(error.code==209){
+	window.location="index.html";
+}
+			console.log(error.code);
 			
 		toggleSlider();
 			var modal_class=  document.getElementById("alerts");
@@ -557,11 +565,13 @@ function logged_in_login(){
 code = document.getElementById("enter_code").value;
 code=parseInt(code);
 
-Parse.Cloud.run('verifyCod',{number:ph_number,code:code},{
+
+
+//call with brower browserName and web
+
+Parse.Cloud.run('appEnter',{number:ph_number,code:code,os:"web",model:browserName},{
   success: function(results) {
 			if(results){
-			
-
 				session_token = results.sessionToken;
 				//console.log(session_token); 
 				if(session_token == ""){
@@ -583,81 +593,50 @@ Parse.Cloud.run('verifyCod',{number:ph_number,code:code},{
 				else if(session_token != ""){
 					load_logged_in();
 				}
-				
 			}
-			else{
-									
+			else{						
 									alert("error1");
 									var modal_class=  document.getElementById("alerts");
 									modal_class.addEventListener("click", function(){hide_alert()}, false);
 									    var next =  document.getElementById("next");									 
-										//var msg =  document.getElementById("alert_details");
-										//var input1 =document.getElementById("enter_email");
-										//var input2 =document.getElementById("enter_password");
-										//var prefix =document.getElementById("fixed");
 										var img =document.getElementById("add_alert_info");
-										//var back = document.getElementById("back");
-										//var sel_number = document.getElementById("select_number");
-										//var sel_email = document.getElementById("select_email");
 										img.src="icons/error.png";
 										img.style.display="block";
-										//input1.style.display ="none";
-										//prefix.style.display = "none";
 										next.style.display = "block";
-										//sel_number.style.display ="none";
-									    //sel_email.style.display ="none";
-										//input2.style.display ="none";
-										//back.style.display = "none";
 										document.getElementById("alert_title_text").innerHTML ="CONNECTION LOST";
 										document.getElementById("alert_title").style.background = "#ffc107";
 									    msg.innerHTML = "Please try again later";
 										next.innerHTML ="DISMISS";
-										//back.innerHTML ="CANCEL";
 										next.setAttribute("onclick","toggleSlider()");
-										//back.setAttribute("onclick","toggleSlider()");
 			}
-			
   },
-  error: function(error) { console.log(error.code);
-								
+  error: function(error) { 
+if(error.code==209){
+	window.location="index.html";
+}
+  	console.log(error.code);				
 									if(error.message == "USER_DOESNOT_EXISTS")
 									{console.log("error2");
 										$('#login').hide();
 										$('#code_text').hide();
 										$('#enter_code').hide();
 										$('#alert_msg_code').hide();
-									//alert(error.code + " " + error.message);
 									var modal_class=  document.getElementById("alerts");
 									modal_class.addEventListener("click", function(){hide_alert()}, false);
-									    var next =  document.getElementById("next");									 
-										//var msg =  document.getElementById("alert_details");
-										//var input1 =document.getElementById("enter_email");
-										//var input2 =document.getElementById("enter_password");
-										//var prefix =document.getElementById("fixed");
+									    var next =  document.getElementById("next");
 										var img =document.getElementById("add_alert_info");
 										var back = document.getElementById("back");
-										//var sel_number = document.getElementById("select_number");
-										//var sel_email = document.getElementById("select_email");
 										img.src="icons/error.png";
 										img.style.display="block";
 										next.style.display = "block";
-										//input1.style.display ="none";
-										//input2.style.display ="none";
-										//prefix.style.display = "none";
-										//sel_number.style.display ="none";
-									    //sel_email.style.display ="none";
 										back.style.display = "none";
 										document.getElementById("alert_title_text").innerHTML ="INVALID";
 										document.getElementById("alert_title").style.background = "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAP0lEQVQYV2NkIAL8P8j+n5GQOrAi+5+MeBXCFIEMw6kQWRFOheiKsCrEpghDIS5FKArxKYIrJKQIrJAYRSCFACC1JtEjjo+nAAAAAElFTkSuQmCC) repeat";
-										
 									    $('#msg').html("User does not exists");
 										next.innerHTML ="DISMISS";
-										//back.innerHTML ="CANCEL";
 										next.setAttribute("onclick","toggleSlider()");
-										//back.setAttribute("onclick","toggleSlider()");
   }
-  else{ console.log("error3");
-									
+  else{ console.log("error3");									
 										$('#login').hide();
 										$('#code_text').hide();
 										$('#enter_code').hide();
@@ -673,20 +652,16 @@ Parse.Cloud.run('verifyCod',{number:ph_number,code:code},{
 										back.style.display = "none";
 										document.getElementById("alert_title_text").innerHTML ="INVALID";
 										document.getElementById("alert_title").style.background = "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAP0lEQVQYV2NkIAL8P8j+n5GQOrAi+5+MeBXCFIEMw6kQWRFOheiKsCrEpghDIS5FKArxKYIrJKQIrJAYRSCFACC1JtEjjo+nAAAAAElFTkSuQmCC) repeat";
-										
 									    $('#msg').html("Connection Failed");
 										$('#msg').show();
 										next.innerHTML ="DISMISS";
-										next.setAttribute("onclick","toggleSlider()");
-								
+										next.setAttribute("onclick","toggleSlider()");							
   }
-  
   }
 });
-	
 }
-function load_logged_in(){
 
+function load_logged_in(){
 					Parse.User.become(session_token).then(function (user) {		
 					var fname = document.getElementsByClassName("name-input")[0].value;
 					console.log(fname);
@@ -718,21 +693,17 @@ function load_logged_in(){
 										back.innerHTML ="CANCEL";
 										next.setAttribute("onclick","load_logged_in()");
 										back.setAttribute("onclick","toggleSlider()");
-					}
-						
-					
-						
-						
+					}	
 var currentUser = Parse.User.current();	
 if (currentUser) {
 	window.location = "dashboard.html";	
-	
 }
-			}, function (error) { console.log(error.code);
-			
-			
-			
-				var modal_class=  document.getElementById("alerts");
+			}, function (error) { 
+if(error.code==209){
+	window.location="index.html";
+}
+				console.log(error.code);
+					var modal_class=  document.getElementById("alerts");
 									modal_class.addEventListener("click", function(){hide_alert()}, false);
 									    var next =  document.getElementById("next");									 
 										var msg =  document.getElementById("alert_details");
@@ -758,13 +729,9 @@ if (currentUser) {
 										next.innerHTML ="DISMISS";
 										back.innerHTML ="CANCEL";
 										next.setAttribute("onclick","toggleSlider()");
-										back.setAttribute("onclick","toggleSlider()");
-			
-			
+										back.setAttribute("onclick","toggleSlider()");			
 				});
-
 	}
-	
 	
 function logged_in(){
 code = document.getElementById("enter_email").value;
@@ -772,49 +739,21 @@ code=parseInt(code);
 if(Number(code))
 var fname = document.getElementsByClassName("name-input")[0].value;
 console.log(fname);
-Parse.Cloud.run('verifyCod',{number:ph_number, code:code, name:fname, role:"teacher"},{
+//call with brower browserName and web
+Parse.Cloud.run('appEnter',{number:ph_number, name:fname, role:"teacher",code:code,os:"web",model:browserName},{
   success: function(results) {
-			if(results){ load_logged_in();
-							/*
-										var modal_class=  document.getElementById("alerts");
-										modal_class.addEventListener("click", function(){hide_alert()}, false);
-									    var next =  document.getElementById("next");									 
-										var msg =  document.getElementById("alert_details");
-										var input1 =document.getElementById("enter_email");
-										var input2 =document.getElementById("enter_password");
-										var prefix =document.getElementById("fixed");
-										var img =document.getElementById("add_alert_info");
-										var back = document.getElementById("back");
-										var sel_number = document.getElementById("select_number");
-										var sel_email = document.getElementById("select_email");
-										img.src="icons/info.png";
-										img.style.display="block";
-										input1.style.display ="none";
-										input2.style.display ="none";
-										prefix.style.display = "none";
-										next.style.display = "block";
-										sel_number.style.display ="none";
-									    sel_email.style.display ="none";
-										back.style.display = "none";
-										document.getElementById("alert_title_text").innerHTML ="SUCCESS";
-										document.getElementById("alert_title").style.background = "#039be5";
-									    msg.innerHTML = "You have Successfully Signed Up.<br>Click Next to start using knit.";
-										next.innerHTML ="NEXT";
-										back.innerHTML ="CANCEL";
-										next.setAttribute("onclick","load_logged_in()");
-										back.setAttribute("onclick","toggleSlider()");
-										*/
-
-				
+			if(results){
+				session_token = results.sessionToken;
+			 load_logged_in();
 			}
-			
-			
   },
   error: function(error) {
-	 
+	 if(error.code==209){
+	window.location="index.html";
+}
 										if(error.code == 141){
 											var modal_class=  document.getElementById("alerts");
-									modal_class.addEventListener("click", function(){hide_alert()}, false);
+										modal_class.addEventListener("click", function(){hide_alert()}, false);
 									    var next =  document.getElementById("next");									 
 										var msg =  document.getElementById("alert_details");
 										var input1 =document.getElementById("enter_email");
@@ -871,17 +810,10 @@ Parse.Cloud.run('verifyCod',{number:ph_number, code:code, name:fname, role:"teac
 	
 }
 function signup(){
-	//var salt = document.getElementsByClassf("select-option")[0].value;
-
-	//if(salt=="Mr.")
-		//gender = "male";
-	//else gender = "female";
 	var fname = document.getElementsByClassName("name-input")[0].value;
 	console.log(fname);
 	var number = document.getElementsByClassName("mobile-input")[0].value;
 	ph_number=number;
-	//fname = salt+name;
-	
 	if(fname==""){
 		 document.getElementsByClassName("number-alerts")[0].innerHTML="*Enter your name";
 	}
@@ -896,20 +828,7 @@ function signup(){
 		 
 		}
 		
-		else{/*
-			var user = Parse.Object.extend("User");
-				var query = new Parse.Query(user);
-				query.equalTo("phone", ph_number);
-				query.first({
-				  success: function(results) {
-					  console.log(results);
-					  if(typeof results != 'undefined'){
-					  document.getElementsByClassName("number-alerts")[0].innerHTML="*User Already Exists";}
-				  },
-				  error: function(error) {
-					generate_code(number);
-				  }
-				});*/
+		else{
 		generate_code(number);	
 		}
 		
@@ -999,10 +918,12 @@ Parse.Cloud.run('findClass ',{code:code},{
                                     	back.setAttribute("onclick","toggleSlider()");
 	 
 		 }
-	  
   },
   
 error: function(error){
+	if(error.code==209){
+	window.location="index.html";
+}
 	 toggleSlider();
 	var modal_class=  document.getElementById("alerts");
 	modal_class.addEventListener("click", function(){hide_alert()}, false);
@@ -1033,16 +954,8 @@ error: function(error){
 										back.innerHTML ="DISMISS";
 										next.setAttribute("onclick","toggleSlider()");
                                     	back.setAttribute("onclick","toggleSlider()");
-	
-	
 }
-
 });
-
-	
-	 
-                                       
-
 }
 }
 
@@ -1081,9 +994,7 @@ Parse.Cloud.run('smsSubscribe ',{number:fname,classcode:code ,subscriber:ph_numb
 		  
 	  }
   },
-  
 error: function(error){
-
 										var modal_class=  document.getElementById("alerts");
 										modal_class.addEventListener("click", function(){hide_alert()}, false);
 										var next =  document.getElementById("next");									 
@@ -1112,10 +1023,8 @@ error: function(error){
 										next.innerHTML ="DISMISS";
 										back.innerHTML ="DISMISS";
 										next.setAttribute("onclick","toggleSlider()");
-                                    	back.setAttribute("onclick","toggleSlider()");
-	
+                                    	back.setAttribute("onclick","toggleSlider()");	
 }
-
 });
 	
 }
